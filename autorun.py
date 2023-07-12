4757ad00c36d4ecedd5a17e51e91621558cfa36b
Rishika Mohanta <neurorishika@gmail.com>
2023-07-12 12:43:27 +0530

    Update autorun.py

--- autorun.py
+++ autorun.py
@@ -2,18 +2,17 @@
 
 import psutil
 import os
-from subprocess import Popen
-import time
-
-# Set the maximum CPU and memory usage
-max_cpu = 0.8
-max_mem = 0.8
+from subprocess import call
 
 def get_cpu_usage():
     return psutil.cpu_percent()
 
 def get_mem_usage():
     return psutil.virtual_memory().percent/100
+
+print("Current System Status:")
+print("CPU Usage: {}%, Memory Usage: {}%".format(get_cpu_usage(), get_mem_usage()))
+
 
 experiments_to_run = [
     ['python','fit_kfold.py','--model', 'QL','--filterdate','2023-06-10'],
@@ -33,23 +32,43 @@ experiments_to_run = [
     ['python','fit_kfold.py','--model', 'AdvLP','--filterdate','2023-06-10'],
 ]
 
-run_till = -1
-processes = []
 while True:
-    # check if cpu and memory usage is below the threshold
-    if get_cpu_usage() < max_cpu and get_mem_usage() < max_mem:
-        # check if all experiments have been run
-        if run_till == len(experiments_to_run)-1:
-            break
-        # run the next experiment
-        run_till += 1
-        print("Running experiment ",run_till)
-        # run the experiment by passing the arguments to Popen and fetch the process id
-        processes.append(Popen(experiments_to_run[run_till]))
+    first_run = input("Run first experiment? (y/n): ") or 'n'
+    if first_run in ['y','n','Y','N']:
+        break
     else:
-        # wait for 10 seconds
-        time.sleep(10)
+        print("Invalid input, please try again")
 
-# wait for all processes to finish
-for p in processes:
-    p.wait()
+if first_run.lower() == 'y':
+    with open('.tmp','w') as f:
+        f.write('0')
+else:
+    # make sure the .tmp file exists
+    if not os.path.exists('.tmp'):
+        print("Error: .tmp file not found, starting from the first experiment")
+        with open('.tmp','w') as f:
+            f.write('0')
+
+# read the .tmp file
+with open('.tmp','r') as f:
+    current_experiment = int(f.read())
+
+# update the .tmp file
+os.remove('.tmp')
+with open('.tmp','w') as f:
+    f.write(str(current_experiment+1))
+
+# run the experiment in a new terminal
+print("Running experiment {}...".format(current_experiment))
+# check os type (windows or linux or mac)
+if os.name == 'nt':
+    # windows
+    call(['start','cmd','/k'] + experiments_to_run[current_experiment])
+elif os.name == 'posix':
+    # linux or mac
+    call(['gnome-terminal','--'] + experiments_to_run[current_experiment])
+else:
+    print("Error: OS not supported")
+    exit(1)
+
+
